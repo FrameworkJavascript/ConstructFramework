405b4aeb72c432d553f258dd7eccd1134bb2e747
Daniell Mesquita <daniellmesquita@protonmail.com>
2022-02-02 16:43:05 -0300

    add build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+echo "Building FrameworkJS for Construct 2..."
+
+echo "Building FrameworkJS.tar.gz..."
+tar -czvf FrameworkJS.tar.gz source
+
+#echo "Building FrameworkJS.capx..."
+#zip -r FrameworkJS.capx source
